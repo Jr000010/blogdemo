1fea190f0537afa58dceeb76472ca5a34a466646
Jr000010 <135720801+Jr000010@users.noreply.github.com>
2024-06-09 14:49:08 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('#menuButton');
+const navMenu = document.querySelector('#navMenu');
+
+toggleMenu = function() {
+    navMenu.classList.toggle("mobileMenuVisible")
+    navMenu.classList.toggle("mobileMenuNotVisible")
+}
